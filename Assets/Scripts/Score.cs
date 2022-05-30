82b40801ea5744b2a1b59354b6824a38ee3fa81c
Balsam <balsam.mm5b@gmail.com>
2022-05-30 16:44:35 +0300

    enemy flying movement

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.AI;
 public class Score : MonoBehaviour
 {
     public float timer;
@@ -11,25 +11,45 @@ public class Score : MonoBehaviour
     public Vector3 Target;
     public int enemyValue;
     public  int scoreLevel=2;
+    /// <summary>
+    ///
+    /// </summary>
+    public float timeForNewPath;
+    bool inCoRoutine;
+    public Rigidbody rb;
+    public float movementSpeed = 10f;
+    public float moveSpeed =100f;
+    public float rotateSpeed = 100f;
 
     public int coinValue;
+
+
+    
+    Vector3 getNewRandomPosition()
+    {
+        float X = Random.Range(-2, 0);
+        //float Y = Random.Range(-1, 1);
+        float Z = Random.Range(-1, 0);
+        Vector3 pos = new Vector3(X,0, Z);
+        return pos;
+    }/*
+    IEnumerator DoSomething()
+    {
+        inCoRoutine = true;
+        yield return new WaitForSeconds(timeForNewPath);
+        GetNewPath();
+        inCoRoutine = false;
+    }
+    void GetNewPath()
+    {
+
+        //transform.position =transform.position+ getNewRandomPosition();
+        //transform.Translate(getNewRandomPosition());
+        //rb.MovePosition(transform.position + (getNewRandomPosition() * movementSpeed * Time.deltaTime));
+        Vector3 pos = getNewRandomPosition();
+        transform.position = pos * Time.deltaTime;
+    }*/
     /*
-    void Start()
-    {
-        
-    }
-
-     void Update()
-    {
-        timer += Time.deltaTime;
-        if (timer>= newtarget)
-        {
-            newTarget();
-            timer = 0;
-        }
-        
-    }
-
     void newTarget()
     {
         float myX = gameObject.transform.position.x;
@@ -41,6 +61,15 @@ public class Score : MonoBehaviour
         Target = new Vector3(xPos, yPos, zPos);
         transform.Translate(Target);
     }*/
+
+    void Update()
+    {
+
+        transform.Translate(getNewRandomPosition() * moveSpeed * Time.deltaTime, Space.World);
+
+        transform.Rotate(0, 0, 1f * rotateSpeed * Time.deltaTime);
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("player"))
--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -27,22 +27,29 @@ public class spawner : MonoBehaviour
     IEnumerator waitSpawner()
     {
         yield return new WaitForSeconds(startWait);
+       int  iter = stop;
         while (stop>0)
         {
            // randEnemy = Random.Range(0, 2);
-            if (stop % 2 == 0)
-            {
+           
                 randEnemy = 0;
-            }
-            else
-            {
+            
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), Random.Range(-spawnValues.z, spawnValues.z));
+            Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), /*gameObject.transform.rotation*/Quaternion.FromToRotation(Vector3.up, transform.forward));
+            yield return new WaitForSeconds(spawnWait);
+            stop = stop - 1;
+        }
 
-                randEnemy = 1;
-            }
+        while (iter > 0)
+        {
+            // randEnemy = Random.Range(0, 2);
+
+            randEnemy = 1;
+
             Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), Random.Range(-spawnValues.z, spawnValues.z));
             Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
             yield return new WaitForSeconds(spawnWait);
-            stop = stop - 1;
+            iter = iter - 1;
         }
     }
 }
